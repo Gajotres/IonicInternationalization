f95ec43ef5eb1d898209bbe82530f15e5d663c1c
Dragan Gaic <dragan.gaic@gmail.com>
2017-10-30 21:37:18 +0100

    DG: The final commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,43 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+export function HttpLoaderFactory(http: HttpClient) {
+    return new TranslateHttpLoader(http);
+}
+
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+    declarations: [
+        MyApp,
+        HomePage
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        IonicModule.forRoot(MyApp),
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient]
+            }
+        })        
+    ],
+    bootstrap: [IonicApp],
+        entryComponents: [
+        MyApp,
+        HomePage
+    ],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        {provide: ErrorHandler, useClass: IonicErrorHandler}
+    ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,22 +1,29 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { TranslateService } from '@ngx-translate/core';
+
 @Component({
 	selector: 'page-home',
 	templateUrl: 'home.html'
 })
+
 export class HomePage {
 
-	constructor(public navCtrl: NavController) {
+	bar = { "test": 'bar' };
 
+	constructor(public navCtrl: NavController, private translateService: TranslateService) {
+
+        /*translateService.setTranslation('en', {
+            IN_APP : 'Internal app translation'
+        });
+
+        translateService.setTranslation('hr', {
+            IN_APP : 'Prijevod unutar aplikacije'
+        });*/
 	}
 
-	segmentChanged(el) {
-		alert(el);
+	segmentChanged(event) {
+		this.translateService.use(event._value);
 	}
-
-	selectedEnemies	() {
-		alert('2');
-	}
-
 }
